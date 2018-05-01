58b39b967b87c709efa936c6f4135623b3b89a69
Simon Thrane Hansen <sthranehansen@gmail.com>
2018-05-01 08:53:53 +0200

    Added unit test of toRad method

--- examples/js/map_projection.js
+++ examples/js/map_projection.js
@@ -13,9 +13,10 @@
 
 	window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
 	Potree.MapTextureManagerSettings.tileServer = "http://tileserver.bnjamin.com/styles/osm-bright/";
+	Potree.MapTextureManagerSettings.maxZoomlevel = 19;
 	//viewer.setEDLEnabled(true);
 	viewer.setFOV(60);
-	viewer.setPointBudget(1*1000*1000);
+	viewer.setPointBudget(5*1000*1000);
 	viewer.loadSettingsFromURL();
 
 	viewer.setDescription("Point cloud courtesy of <a target='_blank' href='https://www.sigeom.ch/'>sigeom sa</a>");
