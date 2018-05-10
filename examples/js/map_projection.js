25177e1be6c211e0387233de74ed08be7743c98d
Benjamin French <benjamin@bnjamin.com>
2018-05-10 13:03:03 +0200

    Added the map projection example

--- examples/js/map_projection.js
+++ examples/js/map_projection.js
@@ -1,16 +1,4 @@
 (function() {
-
-	if (false && 'serviceWorker' in navigator) {
-		navigator.serviceWorker.register('../../serviceworker.js')
-			.then(function (reg) {
-				// registration worked
-				console.log('Registration succeeded. Scope is ' + reg.scope);
-			}).catch(function (error) {
-				// registration failed
-				console.log('Registration failed with ' + error);
-			});
-	}
-
 	window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
 	Potree.MapTextureManagerSettings.tileServer = "http://tileserver.bnjamin.com/styles/osm-bright/";
 	Potree.MapTextureManagerSettings.maxZoomlevel = 15;
@@ -22,10 +10,10 @@
 	viewer.setDescription("Point cloud courtesy of <a target='_blank' href='https://www.sigeom.ch/'>sigeom sa</a>");
 
 	// Load and add point cloud to scene
-	Potree.loadPointCloud("../pointclouds/aarhus/cloud.js", "sigeom.sa", e => {
+	Potree.loadPointCloud("http://aarhus.bnjamin.com/cloud.js", "sigeom.sa", e => {
 		let scene = viewer.scene;
 		let pointcloud = e.pointcloud;
-		//pointcloud.usesMapTexture = true;
+		pointcloud.usesMapTexture = true;
 
 		let material = pointcloud.material;
 		material.size = 1.2;
